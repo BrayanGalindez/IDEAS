5ad345dd8cb646fe8ee5075bd5a313030b07c720
Tabare Bermudez <tabarebermudez@gmail.com>
2023-07-25 12:02:32 -0300

    [FEATURE] Se agrega cardsHelper para poder validar transacciones.

--- Backend/src/controllers/transactionsController.js
+++ Backend/src/controllers/transactionsController.js
@@ -1,10 +1,24 @@
 const { TransactionsObject } = require('../helpers/transactionsHelper')
+const { UsersObject } = require('../helpers/usersHelper')
+const { CardsObject } = require('../helpers/cardsHelper')
 
 exports.getUserTransactions = async (req, res) => {
   try {
     if ('id' in req.query) {
       const response = await TransactionsObject.getUserTransactions(req.query.id)
-      res.status(200).json(response)
+      if (response.length > 0) {
+        // En lugar de devolver los id de la tarjeta, devuelvo el numero
+        for (let i = 0; i < response.length; i++) {
+          const item = response[i]
+          item.tarjeta_origen = await CardsObject.getCardNumberByCardId(item.tarjeta_origen)
+          item.tarjeta_destino = await CardsObject.getCardNumberByCardId(item.tarjeta_destino)
+        }
+        res.status(200).json(response)
+      } else {
+        res.status(400).json({
+          message: 'No se encontraron transacciones para el usuario especificado.'
+        })
+      }
     } else {
       res.status(400).json({
         message: 'Debe incluir id en la peticion'
@@ -17,14 +31,45 @@ exports.getUserTransactions = async (req, res) => {
 
 exports.postUserTransaction = async (req, res) => {
   try {
-    const response = await TransactionsObject.postUserTransaction(req.body)
-    if (response === 1) {
+    // Tiene saldo para hacer la transaccion?
+    const userBalance = await UsersObject.userBalance(req.body.origen_usuario_id)
+    if (userBalance < req.body.monto) {
+      res.status(400).json({
+        message: 'No tiene saldo suficiente.'
+      })
+    }
+
+    // Estan correctos los datos de destino?
+    const userRecipientId = await CardsObject.getUserIdByCardNumber(req.body.tarjeta_destino)
+    if (userRecipientId !== req.body.destino_usuario_id) {
+      res.status(400).json({
+        message: 'Destinatario no valido.'
+      })
+    }
+
+    // La transaccion espera el id de tarjeta y no sus numeros
+    req.body.tarjeta_origen = await CardsObject.getCardIdByCardNumber(req.body.tarjeta_origen)
+    req.body.tarjeta_destino = await CardsObject.getCardIdByCardNumber(req.body.tarjeta_destino)
+
+    // Hacer la transaccion
+    // Primero intento registrar la transaccion
+    const transactionResponse = await TransactionsObject.postUserTransaction(req.body)
+    if (transactionResponse !== 1) {
+      res.status(400).json({
+        message: 'Error al realizar la transaccion. Comuniquese con el banco.'
+      })
+    }
+
+    // Si la transaccion fue exitosa, actualizo el saldo de los usuarios
+    const userBalanceResponse = await UsersObject.changeUserBalance(req.body.origen_usuario_id, -req.body.monto)
+    const userRecipientBalanceResponse = await UsersObject.changeUserBalance(req.body.destino_usuario_id, req.body.monto)
+    if (userBalanceResponse === 1 && userRecipientBalanceResponse === 1) {
       res.status(200).json({
-        message: 'Transaccion realizada con exito'
+        message: 'Transaccion realizada con exito.'
       })
     } else {
       res.status(400).json({
-        message: 'Error al realizar la transaccion'
+        message: 'Error al realizar la transaccion. Comuniquese con el banco.'
       })
     }
   } catch (error) {
