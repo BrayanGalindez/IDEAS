b069270468b4f6e32ac887c871b09db9480fc4cb
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-29 17:24:57 -0500

    [FIX] Se corrige la configuracion de tailwind

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-theme': '#09595A',
+        'color-theme-hover': '#F7C345',
+        'color-terms' : '#0448F8',
+        'color-button-hover' : '#F5B00A'
+      },
+    },
   },
   plugins: [],
 }
