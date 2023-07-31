025c0ce5227be657b7d8dc581256bc5c35a5b5bc
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-31 13:15:57 -0500

    [FEATURE] Creacion del componente de las rutas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Balance from "./pages/Balance";
+import History from "./pages/History";
+import History2 from "./pages/History2";
+import Transfer from "./pages/Transfer";
+import UserProfile from "./components/UserProfile";
+import ConfirmTransfer from "./pages/ConfirmTransfer";
+import Footer from "./components/Footer";
+import Table from "./components/Table";
 function App() {
   return (
-    <div>Hola mundo!</div>
-  )
+    <div className="app">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/balance" element={<Balance />} />
+            <Route path="/historial" element={<History />} />
+            <Route path="/historial2" element={<History2 />} />
+            {/* <Route path="/tabla" element={<Table />} /> */}
+            <Route path="/transfer" element={<Transfer />} />
+            <Route path="/usuario" element={<UserProfile />} />
+            <Route path="/confirmacion" element={<ConfirmTransfer/>} />
+          </Routes>
+          <Footer />
+        </Router>
+      </div>
+  );
 }
 
-export default App
+export default App;
