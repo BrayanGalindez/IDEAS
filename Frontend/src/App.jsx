7bff647a30e7a04bf99c8c11cc7a67885fa32c05
joaquin monzon <joamonz15@gmail.com>
2023-08-05 16:14:29 -0300

    [FEATURE] Agrego la funcion de AutoLogout y agrego funcionalidad al boton de cierre de sesion

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,17 +2,20 @@ import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Balance from "./pages/Balance";
-import History2 from "./pages/History2";
+//import History2 from "./pages/History2";
 import History from "./pages/History";
 import Transfer from "./pages/Transfer";
 import ConfirmTransfer from "./pages/ConfirmTransfer";
 import Footer from "./components/Footer";
 import ClosedSession from "./pages/ClosedSession";
 import CompletedTransaction from "./pages/CompletedTransaction";
+import { AutoLogout } from '../src/components/AutoLogout.jsx'
 function App() {
   return (
     <div className="app">
         <Router>
+          {/* Autologut: la variable sessionTimeout es la cantidad de segundo para el auto logut */}
+          <AutoLogout sessionTimeout={20000}/>
           <Navbar />
           <Routes>
             <Route path="/" element={<Login />} />
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,33 +1,45 @@
 import logo from "../assets/logo.png";
 import { useNavigate } from "react-router-dom";
 function Navbar() {
+  
   const navigate = useNavigate();
+  const data = localStorage.getItem('userData'); 
+  const userData = JSON.parse(data)
+  const handleClearAndReload = async () => {
+    await navigate('/closed');
+    localStorage.clear();
+    window.location.reload();
+  };
+
   return (
 <nav className=" top-0 left-0 right-0 z-50 p-5 bg-color-bg shadow-md">
   <div className="flex items-center justify-between md:w-auto">
     <img className="h-10 cursor-pointer" src={logo} alt="Logo" onClick={() => navigate('/')} />
-    <ul className="justify-end md:flex md:items-center md:static w-full md:w-auto md:bg-opacity-100 transition-all ease-in duration-500 ml-auto">
-      <li className="mx-4 my-6 md:my-0">
-        <a className="text-xl font-[Open Sans] text-white hover:text-color-button duration-500 cursor-pointer"  onClick={() => navigate('/balance')}>
-          Saldo
-        </a>
-      </li>
-      <li className="mx-4 my-6 md:my-0">
-        <a className="text-xl font-[Open Sans] text-white hover:text-color-button  duration-500 cursor-pointer"  onClick={() => navigate('/history')}>
-          Historial
-        </a>
-      </li>
-      <li className="mx-4 my-6 md:my-0">
-        <a className="text-xl font-[Open Sans] text-white hover:text-color-button  duration-500 cursor-pointer"  onClick={() => navigate('/transfer')}  >
-          Transferir
-        </a>
-      </li>
-      <li className="mx-4 my-6 md:my-0">
-        <a className="text-xl font-[Open Sans] text-white hover:text-color-button  duration-500 cursor-pointer"  onClick={() => navigate('/closed')}  >
-          Cerrar sesion
-        </a>
-      </li>
-    </ul>
+    { userData && (
+        <ul className="justify-end md:flex md:items-center md:static w-full md:w-auto md:bg-opacity-100 transition-all ease-in duration-500 ml-auto">
+          <li className="mx-4 my-6 md:my-0">
+            <a className="text-xl font-[Open Sans] text-white hover:text-color-button duration-500 cursor-pointer"  onClick={() => navigate('/balance')}>
+              Saldo
+            </a>
+          </li>
+          <li className="mx-4 my-6 md:my-0">
+            <a className="text-xl font-[Open Sans] text-white hover:text-color-button  duration-500 cursor-pointer"  onClick={() => navigate('/history')}>
+              Historial
+            </a>
+          </li>
+          <li className="mx-4 my-6 md:my-0">
+            <a className="text-xl font-[Open Sans] text-white hover:text-color-button  duration-500 cursor-pointer"  onClick={() => navigate('/transfer')}  >
+              Transferir
+            </a>
+          </li>
+          <li className="mx-4 my-6 md:my-0">
+            <a className="text-xl font-[Open Sans] text-white hover:text-color-button  duration-500 cursor-pointer"  onClick={handleClearAndReload}  >
+              Cerrar sesion
+            </a>
+          </li>
+        </ul>
+      ) 
+    }
   </div>
 </nav>
   );
