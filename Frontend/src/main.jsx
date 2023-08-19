af61262f8323d57ae3dddaf9d941b65f47ab79ec
BrayanGalindez <bgalindez@unal.edu.co>
2023-08-19 17:05:12 -0500

    [FIX] Fixed routes to correctly load links

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import { SesionProvider } from "./provider/SesionProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SesionProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </SesionProvider>
   </React.StrictMode>
 );
