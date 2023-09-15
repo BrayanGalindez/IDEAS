6f6f9c7f3afee5abcc2282d51b78db94273b7984
BrayanGalindez <bgalindez@unal.edu.co>
2023-09-15 12:48:41 -0500

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { SesionProvider } from "./provider/SesionProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SesionProvider>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </SesionProvider>
   </React.StrictMode>
 );
